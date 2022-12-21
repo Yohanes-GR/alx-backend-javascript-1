3be6c2ca88435d52013b8bc0848dd9fdd863370b
Yohanes-GR <yohgut@gmail.com>
2022-12-21 03:26:16 -0500

    remove file

--- 0x0E-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const returnArray = [];
-  for (const element of array) {
-    returnArray.push(appendString + element);
-  }
-
-  return returnArray;
-}
--- 0x0E-ES6_basic/12-createReportObject.js
+++
@@ -1,8 +0,0 @@
-export default function createReportObject(employeesList) {
-  return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
-  };
-}
